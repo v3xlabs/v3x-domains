c23362abe24bc99bc9b4a35e9b1e1b5b12e4d655
Lucemans <luc@lucemans.nl>
2023-08-20 10:08:08 +0000

    Bump Domains

--- components/list.ts
+++ components/list.ts
@@ -7,15 +7,6 @@ export const list = listData['list'] as (
 
 export const list_total = list.length;
 
-// export const list_used = list
-//     .filter((item) => item[0] !== 'Unknown')
-//     .sort((a, b) => a[0].localeCompare(b[0]));
-// export const list_unknown = list
-//     .filter((item) => item[0] === 'Unknown')
-//     .sort((a, b) => a[1].localeCompare(b[1]));
-
-// the above but instead of sorted alphabetically, sorted by length and if lengths are the same alphabetically
-
 export const list_used = list.filter((item) => item[0] !== 'Unknown');
 
 export const list_unknown = list
